761a41c68595b370f0fc67bac3c13d63637d3188
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2014-04-01 17:25:40 +0200

    InfluxDB::Errors are StandardErrors again

--- lib/influxdb/errors.rb
+++ lib/influxdb/errors.rb
@@ -1,5 +1,5 @@
 module InfluxDB
-  class Error < Exception
+  class Error < StandardError
   end
 
   class AuthenticationError < Error
@@ -10,4 +10,23 @@ module InfluxDB
 
   class JSONParserError < Error
   end
+
+  # Taken from
+  # https://github.com/lostisland/faraday/blob/master/lib/faraday/adapter/net_http.rb
+  NET_HTTP_EXCEPTIONS = [
+    EOFError,
+    Errno::ECONNABORTED,
+    Errno::ECONNREFUSED,
+    Errno::ECONNRESET,
+    Errno::EHOSTUNREACH,
+    Errno::EINVAL,
+    Errno::ENETUNREACH,
+    Net::HTTPBadResponse,
+    Net::HTTPHeaderSyntaxError,
+    Net::ProtocolError,
+    SocketError,
+    Zlib::GzipFile::Error,
+  ]
+
+  NET_HTTP_EXCEPTIONS << OpenSSL::SSL::SSLError if defined?(OpenSSL)
 end
