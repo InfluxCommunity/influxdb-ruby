c42ce49e49592250d5c5230ebb681af46e96a529
Dominik Menke <dom@digineo.de>
2016-04-12 20:43:39 +0200

    Update dependencies, add Rubocop
    A .rubocop.yml is already committed, so include it as development
    dependency.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,14 @@ begin
 rescue LoadError
 end
 
+RSpec.configure do |config|
+  config.color = ENV["TRAVIS"] != "true"
+
+  if config.files_to_run.one? || ENV["TRAVIS"] == "true"
+    config.formatter = :documentation
+  else
+    config.formatter = :progress
+  end
+end
+
 InfluxDB::Logging.logger = Logger.new(STDOUT) if ENV['LOG']
