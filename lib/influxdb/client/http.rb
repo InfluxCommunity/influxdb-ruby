b0f10f7630a36a636c169d2cd9a0d032699584f8
Mark Hodson <mark.hodson@qxbranch.com>
2016-07-24 15:09:16 +0200

    Changes to keep Rubocop happy.
    Note: the http.rb module is just over 100 lines. Added exclusion.

--- lib/influxdb/client/http.rb
+++ lib/influxdb/client/http.rb
@@ -4,6 +4,7 @@ require 'net/http'
 require 'net/https'
 
 module InfluxDB
+  # rubocop:disable Metrics/ModuleLength
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Metrics/AbcSize
   module HTTP # :nodoc:
