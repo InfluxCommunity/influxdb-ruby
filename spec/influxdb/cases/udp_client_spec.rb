a4a5c4e044cf055922d23bbaf3f36eabbedadefb
Michael Bensoussan <michael@drivy.com>
2017-01-27 18:47:58 +0100

    General error catching with discard_write_errors

--- spec/influxdb/cases/udp_client_spec.rb
+++ spec/influxdb/cases/udp_client_spec.rb
@@ -16,6 +16,32 @@ describe InfluxDB::Client do
 
       rec_message = s.recvfrom(30).first
       expect(rec_message).to eq message
+
+      s.close
+    end
+  end
+
+  describe "#write with discard_write_errors" do
+    let(:client) do
+      described_class.new(
+        udp: { host: "localhost", port: 44_444 },
+        discard_write_errors: true
+      )
+    end
+
+    it "doesn't raise" do
+      s = UDPSocket.new
+      s.bind("localhost", 44_444)
+
+      client.write_point("responses", values: { value: 5 }, tags: { region: 'eu' })
+      s.close
+
+      client.write_point("responses", values: { value: 7 }, tags: { region: 'eu' })
+
+      allow(client).to receive(:log)
+      expect {
+        client.write_point("responses", values: { value: 7 }, tags: { region: 'eu' })
+      }.not_to raise_error
     end
   end
 end
