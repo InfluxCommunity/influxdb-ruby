67932b353b9ce08729767f6d488f636e65f42c43
Nicholas Volker <45795784+nvolker@users.noreply.github.com>
2022-06-20 13:09:02 +0200

    Revert "Use defined? to avoid warnings"
    This reverts commit 3e3bbf399aaa6a4123fbae4ce822183d819529fb.

--- lib/influxdb/writer/async.rb
+++ lib/influxdb/writer/async.rb
@@ -28,12 +28,12 @@ module InfluxDB
 
       WORKER_MUTEX = Mutex.new
       def worker
-        return @worker if defined? @worker
+        return @worker if @worker
 
         WORKER_MUTEX.synchronize do
           # this return is necessary because the previous mutex holder
           # might have already assigned the @worker
-          return @worker if defined? @worker
+          return @worker if @worker
 
           @worker = Worker.new(client, config)
         end
